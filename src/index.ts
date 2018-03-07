a79a0b302ff4cf1349a80d7d4b1d415c78b88845
Pablo Ibanez <me@pabloibanez.com>
2018-03-07 00:41:56 +0000

    refactoring works

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,28 @@
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { TokenInterceptor } from './token.interceptor';
+import { AuthenticationService } from './authentication.service';
+
 export * from './authentication.service';
 export * from './models/authData';
 export * from './models/login';
 export * from './authGuard';
 export * from './token.interceptor';
+
+@NgModule({
+})
+export class Ng2SmartAuthModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: Ng2SmartAuthModule,
+      providers: [
+        AuthenticationService,
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: TokenInterceptor,
+          multi: true
+        }
+      ]
+    };
+  }
+}
